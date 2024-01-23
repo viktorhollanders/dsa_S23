5e8a4bef28dcc4c773d4d5c3a99a06d3cd7c16e5
Viktor Hollanders <viktorhollanders@Viktors-MacBook-Air.local>
2024-01-23 15:46:48 +0000

    add pizza manager

---
+++ pizza_manager/pizza.py
@@ -0,0 +1,10 @@
+class Pizza:
+    def __init__(self, uid=None, toppings=[]) -> None:
+        self.uid = uid
+        self.toppings = toppings
+        self.served = False
+
+
+pepperonidream = Pizza()
+
+
---
+++ pizza_manager/pizza_manager.py
@@ -0,0 +1,53 @@
+from pizza import Pizza
+
+
+class PizzaManager:
+    def __init__(self):
+        self.pizzas = []
+        self.counter = []
+
+    def add_pizzas(self, toppings):
+        # will add toppings to pizza and store it for the pizza
+        count = 1
+
+        toppings = (
+            input("Input toppings (1-3), write each topping with a space inbetween: ")
+            .replace(" ", ",")
+            .split(",")
+        )
+
+        pizza = Pizza()
+
+        if len(toppings) >= 1 or len(toppings) <= 3:
+            pizza.toppings = toppings
+
+            if count == 1000:
+                pizza.uid = count
+                count = 1
+            count += 1
+
+        else:
+            return None
+
+    def get_all_pizzas(self):
+        # returns all the pizzas
+        return self.pizzas
+
+    def mark_served_pizza(self, id):
+        # marks a pizza served using the id
+        for pizza in self.pizzas:
+            if pizza.uid == id:
+                pizza.served = True
+
+    def remove_all_served_pizzas(self):
+        # removes all served pizza
+        for pizza, index in enumerate(self.pizzas):
+            if pizza.served is True:
+                pizza.pop(index)
+
+
+
+# unit tests
+    # import class into unit tests
+# have a sequens diagram to work by
+# 
